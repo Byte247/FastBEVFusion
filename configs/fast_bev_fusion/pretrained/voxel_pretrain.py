e54ccc115ac05266de6360c06e502167f25d1f3d
Tom Sanitz <tom.sanitz@googlemail.com>
2024-11-22 09:43:31 +0100

    lower lr for voxel

--- configs/fast_bev_fusion/pretrained/voxel_pretrain.py
+++ configs/fast_bev_fusion/pretrained/voxel_pretrain.py
@@ -250,7 +250,7 @@ eval_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=8,
+    samples_per_gpu=4,
     workers_per_gpu=1,
     train=dict(
         type='CBGSDataset',
@@ -276,18 +276,19 @@ input_modality = dict(
     use_map=False,
     use_external=False)
 
-optimizer = dict(type='AdamW', lr=1e-4, weight_decay=0.01)  
+lr = 1e-5
+optimizer = dict(type='AdamW', lr=lr, weight_decay=0.01)  
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
     policy='cyclic',
-    target_ratio=(10, 1e-4),
+    target_ratio=(10, lr),
     cyclic_times=1,
-    step_ratio_up=0.4)
+    step_ratio_up=0.2)
 momentum_config = dict(
     policy='cyclic',
     target_ratio=(0.8947368421052632, 1),
     cyclic_times=1,
-    step_ratio_up=0.4)
+    step_ratio_up=0.2)
 
 # runtime settings
 runner = dict(type='EpochBasedRunner', max_epochs=20)
