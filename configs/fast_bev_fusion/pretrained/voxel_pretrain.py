530dbbb330a722b2653ce76643d1c654aeaa4f89
Tom Sanitz <tom.sanitz@googlemail.com>
2024-11-23 10:51:40 +0100

    use poly lr

--- configs/fast_bev_fusion/pretrained/voxel_pretrain.py
+++ configs/fast_bev_fusion/pretrained/voxel_pretrain.py
@@ -285,15 +285,14 @@ optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
 # learning policy
 lr_config = dict(
-    policy='cyclic',
-    target_ratio=(10, lr),
-    cyclic_times=1,
-    step_ratio_up=0.3)
-momentum_config = dict(
-    policy='cyclic',
-    target_ratio=(0.8947368421052632, 1),
-    cyclic_times=1,
-    step_ratio_up=0.3)
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0,
+    by_epoch=False
+)
 
 # runtime settings
 runner = dict(type='EpochBasedRunner', max_epochs=20)
