9a9fc11a72ecd798605d4847f908f12b58f207d0
Tom Sanitz <tom.sanitz@googlemail.com>
2024-11-30 15:22:01 +0100

    wip

--- configs/fast_bev_fusion/pretrained/dsvt_pretrained_large.py
+++ configs/fast_bev_fusion/pretrained/dsvt_pretrained_large.py
@@ -190,7 +190,7 @@ train_pipeline = [
         file_client_args=file_client_args,
         pad_empty_sweeps=True),
     dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
-    #dict(type='ObjectSample', db_sampler=db_sampler),
+    dict(type='ObjectSample', db_sampler=db_sampler),
     dict(
         type='GlobalRotScaleTrans',
         rot_range=[-0.78539816, 0.78539816],
