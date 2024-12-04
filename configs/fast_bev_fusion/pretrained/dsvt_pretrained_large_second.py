f0da13a6afe77629825a1eedc5031b3a13581de5
Tom Sanitz <tom.sanitz@googlemail.com>
2024-12-04 14:33:54 +0100

    wip

--- configs/fast_bev_fusion/pretrained/dsvt_pretrained_large_second.py
+++ configs/fast_bev_fusion/pretrained/dsvt_pretrained_large_second.py
@@ -200,7 +200,7 @@ train_pipeline = [
         file_client_args=file_client_args,
         pad_empty_sweeps=True),
     dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
-    dict(type='ObjectSample', db_sampler=db_sampler),
+    #dict(type='ObjectSample', db_sampler=db_sampler),
     dict(
         type='GlobalRotScaleTrans',
         rot_range=[-0.78539816, 0.78539816],
@@ -320,7 +320,7 @@ lr_config = dict(
 )
 
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=15)
+runner = dict(type='EpochBasedRunner', max_epochs=20)
 
 
 
